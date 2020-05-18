3e0d6fb0317058915e62193b39073309adf1be42
an5402 <39069602+an5402@users.noreply.github.com>
2020-05-18 17:39:44 -0400

    Update gradient_boost.py

--- Decision Trees and Gradient Boost/gradient_boost.py
+++ Decision Trees and Gradient Boost/gradient_boost.py
@@ -4,9 +4,24 @@ Implements the functionality for gradient boost, a method for estimating a regre
 @author: Artem Naida
 """
 
-# import ...
+import decision_tree as dt
 
 
 class GradientBoost:
-    def __init__(self):
+    """
+    A Gradient Boost algorithm. Uses a series of scaled and leaf-limited decision trees
+    to refine the residuals from a regression.
+    """
+    def __init__(self, input_data, learning_rate, max_number_trees, max_number_leaves):
+        self.data = input_data
+        initial_prediction = dt.fc.np.mean(input_data["tg"])
+        self.data["tg"] = self.data["tg"] - initial_prediction
+        self.trees = [dt.DecisionTreeReg(self.data, max_leaves=max_number_leaves)]
+        # From here on things get weird...
+        for i in range(2, max_number_trees + 2):
+            self.data["tg"] = self.data["tg"] - learning_rate * self.trees[i - 2].predict_point(self.data)
+            self.trees.append(dt.DecisionTreeReg(self.data, max_leaves=max_number_leaves))
+
+    def predict_point(self, point):
+        # Predict the value of a new point using the gradient boost algorithm
         pass
